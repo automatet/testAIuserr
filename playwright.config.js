e90d1e9f7ba39c406c0748a21262e33be532822a
Sharma Himanshu <sharma.himanshu@newvisionsoftpune.com>
2026-02-23 00:03:03 +0530

    added

--- playwright.config.js
+++ playwright.config.js
@@ -29,11 +29,10 @@ export default defineConfig({
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
+    screenshot: 'on',
+    video: 'on',
+    trace: 'on',
     // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
   },
 
   /* Configure projects for major browsers */
